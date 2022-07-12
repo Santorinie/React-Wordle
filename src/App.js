54cf62aa08427b137aa17dcea96bd578a46b8765
Santorinie <tnbendu@gmail.com>
2022-07-12 23:06:15 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+
+class App extends Component {
+  state = {  } 
+
+  render() { 
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +25,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
