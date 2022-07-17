c53f1740ab3b5b27f6a368b60aa41d2735da25e1
Santorinie <tnbendu@gmail.com>
2022-07-17 16:35:46 +0200

    Flowchart added

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import guessLetter from './guessLetter';
 
 class Container extends Component {
     state = {  } 
     render() { 
         return (
-            <div className='gameContainer'></div>
+            <div className='gameContainer'>
+                <div className="headerContainer"></div>
+                <div className="playboardContainer"></div>
+                <div className="letterContainer"></div>
+            </div>
+            
 
         );
     }
 }
  
 export default Container;
-    <div className='gameContainer'></div>
+    
---
+++ src/guessLetter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class guessLetter extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div className='letterHolder'>1</div>
+        );
+    }
+}
+ 
+export default guessLetter;
+<div className='letterHolder'>1</div>
