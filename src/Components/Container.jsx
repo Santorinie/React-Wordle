927e72521c6550de4348394c29ca8cd787d2aa62
Santorinie <tnbendu@gmail.com>
2022-07-20 14:54:43 +0200

    Keyboard implemented

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useCallback } from 'react';
 import GuessLetter from './guessLetter';
 import HeaderContainer from './headerContainer';
 import PlayboardContainer from './playboardContainer';
@@ -6,13 +6,26 @@ import LetterContainer from './letterContainer';
 
 
 class Container extends Component {
-    state = {  } 
-    render() { 
+    state = { 
+        keyboardLetters: [
+            ['Q','W','E','R','T','Y','U','I','O','P'],
+            ['A','S','D','F','G','H','J','K','L'],
+            ['Enter','Z','X','C','V','B','N','M','⌫']
+        ],
+        key: ''
+     } 
+
+     render() { 
+         const handleLetter = (e) =>{
+             console.log("Valami hiba tortent",e);
+             this.setState({key: e});
+         };
+        
         return (
             <div className='gameContainer'>
                 <HeaderContainer></HeaderContainer>
-                <PlayboardContainer></PlayboardContainer>
-                <LetterContainer></LetterContainer>
+                <PlayboardContainer data={this.state.key}></PlayboardContainer>
+                <LetterContainer onChange={handleLetter} keyboardLetters={this.state.keyboardLetters}></LetterContainer>
             </div>
             
 
--- src/Components/letterContainer.jsx
+++ src/Components/letterContainer.jsx
@@ -3,9 +3,17 @@ import React, { Component } from 'react';
 
 class letterContainer extends Component {
     state = {  } 
+
+    
     render() { 
+
+
         return (
-            <div className="letterContainer">hello world</div>
+        <div className='letterContainer'>{
+            this.props.keyboardLetters.map(e => <div key={this.props.keyboardLetters.indexOf(e)} className='lettersRow'>
+            {e.map(l => <div key={this.props.keyboardLetters[this.props.keyboardLetters.indexOf(e)].indexOf(l)} onClick={() => this.props.onChange(l)} className={l.length == 1 ? 'letter' : 'letter specialChar'}>{l}</div>)}
+            </div>)
+        }</div>
         );
     }
 }
